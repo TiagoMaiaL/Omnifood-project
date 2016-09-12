1fabcae1a35cddc3fc54e318fb8a1a6984ac8623
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2016-09-12 00:15:12 -0300

    Adding dynamic scrolling to nav elements.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
   
+  /**
+   * Stycky navigation handling.
+   */
   var waypoint = new Waypoint({
     element: $('.js-section-features'),
     handler: function(direction) {
@@ -14,6 +17,39 @@ $(document).ready(function() {
       }
     },
     offset: '60px'
+  });
+
+  /**
+   * Dynamic Scroll handling.
+   */
+  $('.js-scroll-to-plans').click(function() {
+    $('html, body').animate({
+      scrollTop: $('.js-section-plans').offset().top
+    }, 1000);
+  });
+
+  $('.js-scroll-to-start').click(function() {
+    $('html, body').animate({
+      scrollTop: $('.js-section-features').offset().top
+    }, 1000);
+  });
+
+  /**
+   * Navigation scrolling function.
+   */
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
   });
 
 });
